737edd5b945d8796bec3b4292cad6fcb6ad75335
Adrian <adrian.skulim@gmail.com>
2025-02-11 12:50:56 +0100

    Extend documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! ## Example
+//! Following example demonstrates probing all hosts in a given network range (e.g., an IPv4 subnet).
+//! To run this example locally, make sure to specify the network interface (e.g., `eth0` or `wlan0`) as a parameter.
+//! ```rust
+#![doc = include_str!("../examples/probe.rs")]
+//! ```
+//! In a similar fashion, more advanced ARP requests (e.g., for diagnostic purposes) can be sent using [`client::Client::request`].
+
 pub mod client;
 pub mod error;
 pub mod probe;
